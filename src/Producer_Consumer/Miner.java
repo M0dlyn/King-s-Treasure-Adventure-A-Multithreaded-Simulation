147ecd1145194a2c69a4960fdf7cda18b515f590
Richardzik <kubrama04@gmail.com>
2024-05-03 15:21:10 +0200

    Miner is mining

--- src/Producer_Consumer/Miner.java
+++ src/Producer_Consumer/Miner.java
@@ -17,24 +17,31 @@ public class Miner implements Runnable {
         switch (number) {
             case 0:
                 deposit.put(Valuables.getInstance(ValuablesType.WOODENCOIN));
+                Log.getInstance().logAction("Miner mined the Wooden Coin");
                 break;
             case 1:
                 deposit.put(Valuables.getInstance(ValuablesType.GOLDNUGGET));
+                Log.getInstance().logAction("Miner mined the Gold Nugget");
                 break;
             case 2:
                 deposit.put(Valuables.getInstance(ValuablesType.RUBY));
+                Log.getInstance().logAction("Miner mined the Ruby");
                 break;
             case 3:
                 deposit.put(Valuables.getInstance(ValuablesType.JEWEL));
+                Log.getInstance().logAction("Miner mined the Jewel");
                 break;
             case 4:
                 deposit.put(Valuables.getInstance(ValuablesType.SAPPHIRE));
+                Log.getInstance().logAction("Miner mined the Sapphire");
                 break;
             case 5:
                 deposit.put(Valuables.getInstance(ValuablesType.EMERALD));
+                Log.getInstance().logAction("Miner mined the Emerald");
                 break;
             case 6:
                 deposit.put(Valuables.getInstance(ValuablesType.DIAMOND));
+                Log.getInstance().logAction("Miner mined the DIAMOND");
                 break;
         }
     }
@@ -43,11 +50,13 @@ public class Miner implements Runnable {
     public void run() {
         while (true) {
             try {
+                Log.getInstance().logAction("Miner is mining...");
                 Mine();
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
             try {
+                Log.getInstance().logAction("Miner is sleeping...");
                 Thread.sleep(5000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
--- src/Producer_Consumer/ValuableTransporter.java
+++ src/Producer_Consumer/ValuableTransporter.java
@@ -1,27 +1,53 @@
 package Producer_Consumer;
 
 import Deposit.Deposit;
-import Valuables.Valuables;
+import Valuables.*;
+import java.util.ArrayList;
 
 import java.util.Random;
 
 public class ValuableTransporter implements Runnable {
     private Deposit<Valuables> deposit;
+    private ArrayList valuablesToTransport;
     public ValuableTransporter(Deposit<Valuables> deposit){
         this.deposit = deposit;
+        this.valuablesToTransport = new ArrayList<>();
     }
     private void Take() throws InterruptedException {
         Random randomNumbers = new Random();
         int targetValue = randomNumbers.nextInt(151) + 50;
         int value = 0;
         while(value<targetValue){
+           Log.getInstance().logAction("Valuable Transporter is taking...");
            Valuables takenValuable = deposit.take();
-           takenValuable.getValue();
+           Log.getInstance().logAction("Valuable Transporter took valuable of the value: " + takenValuable.getValue());
+           valuablesToTransport.add(takenValuable);
+           int takenValue = takenValuable.getValue();
+           value += takenValue;
         }
+        valuablesToTransport.clear();
+
+        /* TODO Here we need to make the list for the transporter so the taken goods are saved
+         After transporting the valuables the list needs to be cleared and the
+         Thread needs to sleep and then continue his job */
+
     }
 
     @Override
     public void run() {
+        while(true){
+            try {
+                Take();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            try {
+                Log.getInstance().logAction("Valuable Transporter is sleeping...");
+                Thread.sleep(15000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
 
     }
 }
